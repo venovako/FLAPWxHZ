ba99b8c36f9cb541f834acc921efacadf695873e
Vedran Novakovic <venovako@venovako.eu>
2018-09-23 01:57:00 +0200

    Use ln instead of mv.

--- src/phase2/phase2.sh
+++ src/phase2/phase2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -z "$1" ]
 then
-    echo "$0 FN"
+	echo "$0 FN"
 else
-    mv -fv $1.YY $1.Y
-    mv -fv $1.WW $1.W
-    mv -fv $1.JJ $1.J
+	ln -fv $1.YY $1.Y
+	ln -fv $1.WW $1.W
+	ln -fv $1.JJ $1.J
 fi
