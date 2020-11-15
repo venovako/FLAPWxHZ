5a049f5bbf0fc0352c460db1578c4257db706f45
Vedran Novakovic <venovako@venovako.eu>
2020-11-15 02:25:22 +0100

    Update the makefiles.

--- src/x64.mk
+++ src/x64.mk
@@ -5,17 +5,38 @@ DEBUG=
 else # DEBUG
 DEBUG=g
 endif # ?NDEBUG
+ifndef FP
+ifdef NDEBUG
+FP=source
+else # DEBUG
+FP=strict
+endif # ?NDEBUG
+endif # !FP
 RM=rm -rfv
 AR=xiar
 ARFLAGS=-qnoipo -lib rsv
 CC=icc
 FC=ifort
 CPUFLAGS=-DUSE_INTEL -DUSE_X64 -fPIC -fexceptions -fno-omit-frame-pointer -qopenmp -rdynamic
+ifneq ($(ARCH),Darwin)
+CPUFLAGS += -qopenmp-threadprivate=compat
+endif # Linux
 ifdef PROFILE
 CPUFLAGS += -DVN_PROFILE=$(PROFILE) -fno-inline -finstrument-functions
 endif # PROFILE
 FORFLAGS=$(CPUFLAGS) -i8 -standard-semantics -threads
 C18FLAGS=$(CPUFLAGS) -std=c18
+FPUFLAGS=-fp-model $(FP) -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high
+ifeq ($(FP),strict)
+FPUFLAGS += -fp-stack-check -fimf-arch-consistency=true
+else # !strict
+FPUFLAGS += -fimf-use-svml=true
+endif # ?strict
+FPUFFLAGS=$(FPUFLAGS)
+FPUCFLAGS=$(FPUFLAGS)
+ifeq ($(FP),strict)
+FPUFFLAGS += -assume ieee_fpe_flags
+endif # strict
 ifdef NDEBUG
 OPTFLAGS=-O$(NDEBUG) -xHost -qopt-zmm-usage=high
 OPTFFLAGS=$(OPTFLAGS) -DMKL_DIRECT_CALL
@@ -23,22 +44,16 @@ OPTCFLAGS=$(OPTFLAGS)
 DBGFLAGS=-DNDEBUG -qopt-report=5 -traceback -diag-disable=10397
 DBGFFLAGS=$(DBGFLAGS)
 DBGCFLAGS=$(DBGFLAGS) -w3 -diag-disable=1572,2547
-FPUFLAGS=-fma -fp-model source -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high -fimf-use-svml=true
-FPUFFLAGS=$(FPUFLAGS)
-FPUCFLAGS=$(FPUFLAGS)
 else # DEBUG
 OPTFLAGS=-O0 -xHost -qopt-zmm-usage=high
 OPTFFLAGS=$(OPTFLAGS)
 OPTCFLAGS=$(OPTFLAGS)
 DBGFLAGS=-$(DEBUG) -debug emit_column -debug extended -debug inline-debug-info -debug pubnames -traceback -diag-disable=10397
-ifneq ($(ARCH),Darwin) # Linux
+ifneq ($(ARCH),Darwin)
 DBGFLAGS += -debug parallel
-endif # ?Linux
+endif # Linux
 DBGFFLAGS=$(DBGFLAGS) -debug-parameters all -check all -warn all
 DBGCFLAGS=$(DBGFLAGS) -check=stack,uninit -w3 -diag-disable=1572,2547
-FPUFLAGS=-fp-model strict -fp-stack-check -fma -no-ftz -no-complex-limited-range -no-fast-transcendentals -prec-div -prec-sqrt -fimf-precision=high
-FPUFFLAGS=$(FPUFLAGS) -assume ieee_fpe_flags
-FPUCFLAGS=$(FPUFLAGS)
 endif # ?NDEBUG
 LIBFLAGS=-DUSE_MKL -DMKL_ILP64 -I. -I../../../JACSD/vn -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include
 LDFLAGS=-L../../../JACSD -lvn$(PROFILE)$(DEBUG)
